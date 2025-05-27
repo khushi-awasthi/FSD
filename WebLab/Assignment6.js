5ddf102f6a584ab40e03b742cfc05f05a9d68e79
khushi-awasthi <khushiawasthi8834@gmail.com>
2025-05-27 13:32:15 +0530

    full stack web dev

---
+++ WebLab/Assignment6.js
@@ -0,0 +1,30 @@
+// Assignment -6
+
+// Question 1: Write a program for add two number
+// using function.
+ <script>
+    function sum(a,b){
+        
+    }
+
+ </script>
+
+
+
+// Question 2: Write a program for checking odd or
+// even number.
+
+// Question 3: Write a program for factorial of any
+// number using function.
+
+// Question 4: Write a program for checking number is
+// prime or not number using function.
+
+// Question 5: Write a program for reverse a string
+// using function.
+
+// Question 6: Write a program for palindrome a
+// number using function.
+
+// Question 7: Write a JavaScript function called calculateArea
+// that takes two arguments length and width, and
